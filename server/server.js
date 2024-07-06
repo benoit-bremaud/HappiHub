9fe40ab65174ebcdf11cbc2b4b1f0e64421d1bd1
benoit-bremaud <benoit.bremaud@laplateforme.io>
2024-07-07 00:26:53 +0200

    feat: configure client and server directories, Docker setup
    - Configure package.json files for client, server, and root directories
    - Add nodemon as a dev dependency for server
    - Update Dockerfile and docker-compose.yml to match project structure
    - Create necessary directories and files in server (controllers, models, routes, config)
    - Ensure correct module imports/exports in server files
    - Add and configure environment variables for MongoDB connection
    - Verify and fix module paths to ensure proper functioning of backend

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import connectDB from './config/mongo.js';
+import dotenv from 'dotenv';
+import express from 'express';
+import userRoutes from './routes/userRoutes.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
