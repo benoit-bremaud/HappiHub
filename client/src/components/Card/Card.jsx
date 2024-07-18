773de29b544d115635702035574e4b43aa46c84b
viktor-argirov <viktor.argirov@laplateforme.io>
2024-07-18 15:15:34 +0200

    Home update

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -2,22 +2,24 @@ import React from "react";
 import "./card.css";
 import PropTypes from "prop-types";
 
-const Card = ({ event }) => {
-
-    return (
-        <div className="card">
-            <h1>{event.title}</h1>
-            <img src="https://picsum.photos/id/238/300/300" alt="image de presentation"/>
-            <p>{event.description}</p>
-            <p>{event.date}</p>
-            <p>{event.location}</p>
-            {/* <button>button</button> */}
-        </div>
-    )
+const Card = ({ event,onClick }) => {
+  return (
+    <div className="card" onClick={() => onClick(event)}>
+      <h1 className="card_title">{event.title}</h1>
+      <img src={event.image || "https://picsum.photos/id/238/300/300"} alt="Event" className="card_image" />
+      <div className="card_content">
+        <p className="card_description">{event.description}</p>
+        <p className="card_date">{event.date}</p>
+        <p className="card_location">{event.location}</p>
+      </div>
+    </div>
+  );
 };
 
-Event.propTypes = {
-    event: PropTypes.object,
-  };
+Card.propTypes = {
+  event: PropTypes.object.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default Card;
+
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,17 +1,112 @@
 import React from "react";
 import "./home.css";
+import { useNavigate } from "react-router-dom";
 import Card from "../Card/Card";
 import PropTypes from "prop-types";
 
+// Sample event data
+const events = [
+  {
+    id: 1,
+    title: "Concert in the Park",
+    date: "2024-07-20",
+    description: "Enjoy live music at the park with friends and family.",
+    image: "concert.jpg",
+  },
+  {
+    id: 2,
+    title: "Art Exhibition",
+    date: "2024-08-05",
+    description: "Explore contemporary art from local artists.",
+    image: "art_exhibition.jpg",
+  },{
+    id: 3,
+    title: "Food Festival",
+    date: "2024-09-12",
+    description: "Taste the best dishes from around the world.",
+    image: "food_festival.jpg",
+    location: "Downtown",
+  },
+  {
+    id: 4,
+    title: "Tech Conference",
+    date: "2024-10-22",
+    description: "Join the latest discussions in technology and innovation.",
+    image: "tech_conference.jpg",
+    location: "Convention Center",
+  },
+  {
+    id: 5,
+    title: "Marathon",
+    date: "2024-11-05",
+    description: "Run with thousands of participants in our annual marathon.",
+    image: "marathon.jpg",
+    location: "City Streets",
+  },
+  {
+    id: 6,
+    title: "Book Fair",
+    date: "2024-12-01",
+    description: "Discover new books and meet your favorite authors.",
+    image: "book_fair.jpg",
+    location: "Exhibition Hall",
+  },
+  {
+    id: 7,
+    title: "Film Festival",
+    date: "2025-01-15",
+    description: "Watch premieres of the latest indie films.",
+    image: "film_festival.jpg",
+    location: "Cinema Plaza",
+  },
+  {
+    id: 8,
+    title: "Yoga Retreat",
+    date: "2025-02-20",
+    description: "Relax and rejuvenate with our weekend yoga retreat.",
+    image: "yoga_retreat.jpg",
+    location: "Mountain Resort",
+  },
+  {
+    id: 9,
+    title: "Charity Gala",
+    date: "2025-03-10",
+    description: "Attend our annual gala to support local charities.",
+    image: "charity_gala.jpg",
+    location: "Grand Hotel",
+  },
+  {
+    id: 10,
+    title: "Gardening Workshop",
+    date: "2025-04-05",
+    description: "Learn how to grow your own garden.",
+    image: "gardening_workshop.jpg",
+    location: "Community Center",
+  },
+  // Add more events as needed
+];
+
 const Home = ({ user }) => {
+
+  const navigate = useNavigate();
+
+  const handleCardClick = (event) => {
+    if (user) {
+      navigate(`/event/${event.id}`);
+    } else {
+      navigate("/login", { state: { from: { pathname: `/event/${event.id}` } } });
+    }
+  };
   return (
     <div className="home">
-      <h1>Bienvenue, {user ? user.name : ""} !</h1>
+      <h1>Bienvenue, {user ? user.name : "Visiteur"} !</h1>
       <div className="card_display">
-
+        {events.map((event) => (
+          <Card key={event.id} event={event} onClick={handleCardClick} />
+        ))}
       </div>
     </div>
-  )
+  );
 };
 
 Home.propTypes = {
