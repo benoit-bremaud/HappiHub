ef1c098825ae0500addc08617bd8461723a26447
viktor-argirov <viktor.argirov@laplateforme.io>
2024-07-19 15:31:45 +0200

    .

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import "./home.css";
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
+const Home = ({ user }) => {
+  const navigate = useNavigate();
 
+  const handleNavigate = (path) => {
+    if (user) {
+      navigate(path);
+    } else {
+      navigate("/login", { state: { from: { pathname: path } } });
+    }
+  };
 
-const Home = () => {
   return (
     <div className="home">
-      <h1>Bienvenue!</h1>
-
+      <h1 className="welcome-message">Bienvenue, {user ? user.name : "Visiteur"} !</h1>
+      <p className="sub-text">
+        Découvrez, créez et participez à des événements passionnants dans votre communauté.
+      </p>
+      <div className="actions">
+        <button className="btn discover-btn" onClick={() => handleNavigate("/discover")}>Découvrir</button>
+        <button className="btn create-btn" onClick={() => handleNavigate("/create")}>Créer</button>
+        <button className="btn participate-btn" onClick={() => handleNavigate("/participate")}>Participer</button>
+      </div>
     </div>
-  )
+  );
 };
 
-
+Home.propTypes = {
+  user: PropTypes.object,
+};
 
 export default Home;
